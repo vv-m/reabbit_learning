9e36def5ef76cda80ff37e9a3e57c0a075973795
Vlad Mironov <73903449+vv-m@users.noreply.github.com>
2024-11-23 11:51:04 +0300

    Изменил запуск приложения с помощью lifespan

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-
 from api.routers import router
-
 from brokers.rabbit_broker import broker
 
 
-app = FastAPI(title="FastAPI & Kafka (KRaft)")
-# Подключение маршрутов FastAPI
+# Определяем жизненный цикл приложения с помощью декоратора @asynccontextmanager
+# Этот метод позволяет управлять ресурсами, которые нужно запускать и закрывать с приложением
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # noqa
+    await broker.start()  # Запуск Kafka брокера при старте приложения
+    # scheduler.start()  # Запуск планировщика задач
+    yield
+    await broker.close()  # Закрытие Kafka брокера при остановке приложения
+    # scheduler.shutdown()  # Завершение работы планировщика задач
+
+
+app = FastAPI(title="FastAPI & Rabbit", lifespan=lifespan)
 app.include_router(router)
-
-
-# Подключение FastStream к FastAPI
-@app.on_event("startup")
-async def startup_event():
-    await broker.start()
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    await broker.close()
